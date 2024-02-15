eb6cd54e960fe32982877cd6a590ca30a7070f42
Ksenia <baranova-ksenia1990@gmail.com>
2024-02-15 10:26:57 +0200

    create routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,3 +1,20 @@
+import { Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Home from "./pages/Home"
+import Profile from "./pages/Profile"
+import Signin from "./pages/Signin"
+import Signup from "./pages/Signup"
+
 export default function App() {
-	return <h1 className="text-3xl font-bold underline">Hello world!</h1>
+	return (
+		<>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/signin" element={<Signin />} />
+				<Route path="/signup" element={<Signup />} />
+				<Route path="/profile" element={<Profile />} />
+			</Routes>
+		</>
+	)
 }
